f3de29508e1c66cdcd3289b4ddc63d80d77433fa
Ossama Ghaffar <uk352351@gmail.com>
2024-05-02 22:04:06 +0500

    Inital startup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Mission from "./components/Mission";
+import Navbar from "./components/Navbar";
+import Product from "./components/Product";
+import Sponsor from "./components/Sponsor";
+
+const App = () => {
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <Hero />
+      <Mission />
+      <Product />
+      <Sponsor />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
